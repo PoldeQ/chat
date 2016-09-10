92bd83d62ad120f234f9ac9402609ab806ed4e39
poldeq <gieniusz1337@gmail.com>
2016-09-10 17:15:17 +0200

    fixed session.ex here

--- web/models/session.ex
+++ web/models/session.ex
@@ -3,14 +3,17 @@ defmodule Chat.Session do
   alias Chat.User
 
   def login(params) do
-    user = Repo.get_by!(User, %{email: String.downcase(params["email"])})
+    user = Repo.get_by(User, email: String.downcase(params["email"]))
     case authenticate(user, params["password"]) do
-       true -> {:ok, user}
-       _ -> :error
+      true -> {:ok, user}
+      _    -> :error
     end
   end
 
   defp authenticate(user, password) do
-    Comeonin.Bcrypt.checkpw(password, user.crypted_password)
+    case user do
+      nil -> false
+      _   -> Comeonin.Bcrypt.checkpw(password, user.crypted_password)
+    end
   end
 end
